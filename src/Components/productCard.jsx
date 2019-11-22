93be20148d50b44c653f94de717da14035fe83a2
yasserhous <mike_9125@yahoo.ca>
2019-11-22 14:18:48 -0500

    added addToCart method. added new props for productCard component

--- src/Components/productCard.jsx
+++ src/Components/productCard.jsx
@@ -5,9 +5,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.css.map';
 import {Link} from "react-router-dom";
 
+
 class ProductCard extends Component {
-   state = {
-      quantity:0
+   constructor(){
+      super();
+   
+      this.state = {
+         quantity:0,
+         product:this.props,
+      }
+      this.addToCart = this.addToCart.bind(this)
    }
 
    handleIncrement = () =>{
@@ -16,8 +23,28 @@ class ProductCard extends Component {
    handleDecrement = () =>{
       this.setState({quantity: this.state.quantity - 1});
    }
+   componentDidMount(){
+      this.setState(
+         {
+         product:this.props.product,
 
-
+         }
+      )
+   }
+   addToCart(){
+     // this.productsToCart.push(this.state.product)
+     /* this.setState({
+         productsToCart: this.state.productsToCart.push(this.state.product)
+      })*/
+      
+     // console.log(this.productsToCart);
+      localStorage.setItem("cart",JSON.stringify(this.state.product))
+      let temp=[];
+      temp.push(JSON.parse(localStorage.getItem("cart")))
+      //let temp1 = JSON.parse(temp);
+      temp.push(this.state.product)
+      console.log(temp);
+  }
    render(){
 
     var href = "/productPage/" + this.props.id;
@@ -36,7 +63,7 @@ class ProductCard extends Component {
                <button className="incrementCart" onClick={this.handleIncrement}><span className="glyphicon glyphicon-chevron-right"></span></button>
                <button className="decrementCart" onClick={this.handleDecrement} disabled={this.state.quantity === 0}><span className="glyphicon glyphicon-chevron-left"></span></button>
                <input type="number" className="numItems" value={this.state.quantity}></input>
-               <p><button className="addToCart">Add to Cart</button></p>
+               <p><button className="addToCart" onClick={this.addToCart}>Add to Cart</button></p>
                </div>
 
             </div>
