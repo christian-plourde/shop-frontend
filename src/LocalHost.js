2aa64e6599373cbf06904870823275603ecb52a8
TheyCallMeJQ <jacques.a@libofthings.org>
2019-11-27 23:27:31 -0500

    Fixed localhost

--- src/LocalHost.js
+++ src/LocalHost.js
@@ -1,6 +1,6 @@
 /*
   This variable is being imported in whichever files require a link switch from local to heroku. On local testing (on localhost), set to true, and on submission, set to false.
 */
-const localhost = true;
+const localhost = false;
 
 export default localhost;
