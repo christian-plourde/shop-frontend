1b59180184aca238b2201225592ad728e110966b
TheyCallMeJQ <jacques.a@libofthings.org>
2019-11-28 17:33:34 -0500

    fixed localhost to work on heroku

--- src/LocalHost.js
+++ src/LocalHost.js
@@ -1,7 +1,7 @@
 /*
   This variable is being imported in whichever files require a link switch from local to heroku. On local testing (on localhost), set to true, and on submission, set to false.
 */
-const localhost = true;
+const localhost = false;
 /*
   Usage example (LandingPage.js):
   import localhost from './Localhost.js';
