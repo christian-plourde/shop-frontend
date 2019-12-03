f8ddef833e2c2d2b5bb9d2c93338e1d5455f7c74
Christian Plourde <christian.bertin.plourde@gmail.com>
2019-12-03 00:52:29 -0500

    added style to JQ's ugly mess

--- src/Components/Client/NewProductPosting.js
+++ src/Components/Client/NewProductPosting.js
@@ -4,9 +4,22 @@ import {BrowserRouter as Router, Link as ReactLink} from "react-router-dom";
 //A variable to make our lives easier
 
 function ProductPosting () {
+
+	const button_style =
+    {
+      margin: "0 auto",
+      marginTop: "20px",
+      border: "3px solid #333",
+      padding: "5px",
+      borderRadius: "10px",
+      backgroundColor: "whitesmoke",
+      color: "#333",
+      fontSize: '15px'
+    };
+
   return (
     <ReactLink to={"/productPosting"/*product posting page*/}>
-      <button>
+      <button style = {button_style}>
         <h1>New product posting</h1>
       </button>
     </ReactLink>
